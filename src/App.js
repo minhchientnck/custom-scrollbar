0200936600f53576107ece2b661b34a029373904
minhchiencktv <minhchientnck@gmail.com>
2020-04-03 00:19:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ScrollBar from './scrollbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <ScrollBar className="App">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione perferendis provident velit qui odit nihil, consectetur dolores repudiandae amet libero. Facilis iusto, aut pariatur ad doloribus minima ea alias!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione perferendis provident velit qui odit nihil, consectetur dolores repudiandae amet libero. Facilis iusto, aut pariatur ad doloribus minima ea alias!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione perferendis provident velit qui odit nihil, consectetur dolores repudiandae amet libero. Facilis iusto, aut pariatur ad doloribus minima ea alias!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione perferendis provident velit qui odit nihil, consectetur dolores repudiandae amet libero. Facilis iusto, aut pariatur ad doloribus minima ea alias!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione perferendis provident velit qui odit nihil, consectetur dolores repudiandae amet libero. Facilis iusto, aut pariatur ad doloribus minima ea alias!
+        </ScrollBar>
+        <ScrollBar className="App">
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem in quis autem cupiditate earum suscipit vel distinctio voluptatibus ipsum aliquid natus placeat, sapiente voluptates dicta dolores esse inventore ipsam fugiat!
+            Nobis accusamus voluptatibus exercitationem doloribus tenetur provident, fugiat, eveniet, delectus blanditiis magnam reiciendis ducimus est doloremque maxime aliquid dolorem aperiam alias repudiandae molestiae et quam non. Asperiores consequuntur veniam dolore?
+            Reiciendis dicta in ipsam dolorum quia sed mollitia ex ipsa voluptates reprehenderit ratione consequatur dolorem qui, nam quaerat nulla. Fugit commodi repellendus maiores recusandae obcaecati ab sapiente tempora quas eum?
+            Libero et vel porro cupiditate, quasi mollitia excepturi, minima dolorem, cum sapiente in quo modi obcaecati eaque magnam est blanditiis expedita sequi. Commodi, quidem eos. Dolore fugiat rerum voluptatem vel.
+            Explicabo iusto amet dicta, ab maiores cum aut quos ipsa necessitatibus quas debitis est alias autem reiciendis omnis praesentium doloribus voluptas quidem unde sint quibusdam tenetur consectetur animi. Eos, praesentium!
+            In odit qui cumque beatae laborum expedita placeat voluptatibus aliquam eligendi aperiam quas recusandae pariatur, alias quos provident. Beatae explicabo aliquid eius voluptatibus perspiciatis velit recusandae dolorum accusantium nam cupiditate?
+            Impedit a, voluptates totam itaque nesciunt fuga deserunt quod labore possimus necessitatibus obcaecati culpa beatae, pariatur id suscipit aut harum ut tempora dolorem temporibus natus fugiat mollitia ullam reiciendis. Hic.
+            Aliquam neque consequuntur beatae corporis laboriosam veniam sint facere, in dolorem, porro expedita asperiores eaque, enim sunt debitis a ipsam fuga nemo saepe tempora omnis optio totam provident! Quaerat, architecto!
+            Eos modi, ipsa incidunt dicta culpa nulla distinctio? Consequuntur reiciendis totam id, sunt mollitia cumque voluptate ab fugiat ea sapiente veritatis laudantium libero deleniti quia commodi repudiandae illo eaque amet?
+            Cumque sint possimus similique tempore eius. Maxime non vel, expedita voluptate nemo assumenda obcaecati tenetur at consequuntur sunt ex distinctio illum porro? Saepe enim praesentium blanditiis minus officiis velit explicabo!
+            Perspiciatis iure quidem blanditiis sed vel provident iusto quia incidunt unde eos at minus, atque reprehenderit quam delectus sint veritatis eum numquam molestiae et sapiente? Soluta possimus amet atque ratione?
+            Quae voluptates quibusdam neque earum laboriosam? Animi ipsum neque similique voluptatum nostrum rerum necessitatibus, a atque itaque, molestias, unde quaerat fugit aliquam at cupiditate ab adipisci sapiente debitis suscipit ipsa?
+            Obcaecati quia exercitationem rem delectus, dicta fuga commodi repudiandae suscipit veritatis assumenda nulla atque laboriosam totam, unde blanditiis mollitia quibusdam aut, voluptas iure nemo! Maiores dolore corporis accusantium sit tempora?
+            Recusandae exercitationem ex atque assumenda soluta. Repellendus nihil autem nam, facilis magnam libero id odit dolores in aliquid aliquam minima mollitia enim fugit? Quaerat, unde illum modi porro reiciendis aut.
+            Accusantium repellendus et, culpa animi ipsa sed nulla quasi vel illum beatae aliquam necessitatibus mollitia sit! Recusandae eum dicta quis nam eius! Nostrum similique illo est quasi quia veritatis inventore?
+            Veniam, sequi. Cupiditate quo aut numquam labore qui accusamus eius aspernatur nobis velit ut eligendi maiores culpa, mollitia voluptatibus temporibus blanditiis officia odio voluptas perferendis. Natus placeat voluptates quisquam velit.
+            Omnis suscipit optio, neque repellendus dolorem pariatur quo aspernatur, ullam eaque sed nemo ut esse possimus, magni repellat! Eaque deleniti amet nihil perspiciatis ex eveniet sed vitae natus nam iure.
+            Ducimus illum nulla ipsa molestias et recusandae ut! Earum labore reprehenderit tempora perferendis esse sapiente molestias impedit, aut hic laborum, quidem id minima animi. Sequi eligendi aliquam voluptate sint ab.
+            Nostrum doloribus quisquam, eaque incidunt molestiae mollitia maxime esse natus distinctio id porro sint, animi quaerat! Deleniti ducimus sunt obcaecati velit nesciunt accusamus rem! Dolor, porro. Alias assumenda architecto excepturi.
+            Laboriosam minus ea reprehenderit harum aut sequi nihil! Nobis in autem repudiandae reiciendis quia eveniet, perspiciatis voluptatibus eligendi exercitationem praesentium blanditiis corrupti iusto? Exercitationem veniam cumque itaque id. Laudantium, ex?
+          </div>
+        </ScrollBar>
+    </div>
+  );
+}
+
+export default App;
